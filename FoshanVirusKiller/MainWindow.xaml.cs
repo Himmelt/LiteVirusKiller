58fb71388572f7610f491e12ef88dac3e5e3303e
Himmelt <master@void-3.cn>
2018-03-04 13:54:54 +0800

    add
    fix #2
    fix #3

--- FoshanVirusKiller/MainWindow.xaml.cs
+++ FoshanVirusKiller/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace FoshanVirusKiller
             100,     // autorun.inf
             //347,     // disk.lnk
             480768,  // svhost.exe
+            376832,  // fold.exe
             32768
         };
         private static List<string> VHASH = new List<string>{
@@ -32,6 +33,7 @@ namespace FoshanVirusKiller
             "82-20-57-F5-57-CD-E0-DB-19-BC-22-06-71-54-1B-91-65-83-A1-E8",// autorun.inf
             //"89-B4-3C-C9-B0-D9-93-3C-4E-F7-50-BC-6D-E7-F7-28-8A-8F-7F-45",// disk.lnk
             "ED-02-7B-13-7F-D7-31-04-B3-AD-DD-EF-9E-46-13-0F-03-FB-B6-36",// svhost.exe
+            "1F-6F-E8-49-39-DA-40-51-CE-47-B8-11-30-82-36-65-29-8A-2B-60",// fold.exe
             "06-29-3D-EA-80-E3-9C-7E-B7-EE-2B-DB-00-D6-0B-58-D9-32-FA-8A" // rundll32.exe
         };
 
