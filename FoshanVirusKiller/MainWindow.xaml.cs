6a4aae47cb95ba1a71eda7845e94e92e697a65b8
Himmelt <master@void-3.cn>
2018-03-04 13:08:02 +0800

    ok

--- FoshanVirusKiller/MainWindow.xaml.cs
+++ FoshanVirusKiller/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,12 +19,22 @@ namespace FoshanVirusKiller
     {
 
         private static string KEYFOLDER = @"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
-        private static List<long> SIZES = new List<int> {
-            32768,665
+        private static List<long> SIZES = new List<long> {
+            237568,  // DeviceConfigManager
+            469,     // DeviceConfigManager.vbs
+            100,     // autorun.inf
+            347,     // disk.lnk
+            32768
         };
         private static List<string> VHASH = new List<string>{
-            "06293dea80e39c7eb7ee2bdb00d60b58d932fa8a"
+            "F0-EA-23-8D-9C-6C-C8-67-F1-6C-26-48-8F-D2-2E-FC-40-00-71-D0",// DeviceConfigManager.exe
+            "C7-AD-86-7B-3B-79-E0-20-0C-72-5D-E9-83-DA-79-89-CE-BC-50-E5",// DeviceConfigManager.vbs
+            "82-20-57-F5-57-CD-E0-DB-19-BC-22-06-71-54-1B-91-65-83-A1-E8",// autorun.inf
+            "89-B4-3C-C9-B0-D9-93-3C-4E-F7-50-BC-6D-E7-F7-28-8A-8F-7F-45",// disk.lnk
+            "06-29-3D-EA-80-E3-9C-7E-B7-EE-2B-DB-00-D6-0B-58-D9-32-FA-8A" // rundll32.exe
         };
+
+        private static List<Task> TASKS = new List<Task>();
 
         private static SHA1CryptoServiceProvider SHA1 = new SHA1CryptoServiceProvider();
         private static Action<TextBox> ClearText = new Action<TextBox>((textBox) => textBox.Clear());
@@ -39,6 +50,24 @@ namespace FoshanVirusKiller
         });
         private static Action<Button, bool> EnableButton = new Action<Button, bool>((button, enable) => button.IsEnabled = enable);
 
+        private void DeleteFile(FileInfo target, bool top)
+        {
+            Task task = Task.Factory.StartNew(() =>
+            {
+                target.Attributes = FileAttributes.Normal;
+                try
+                {
+                    target.Delete();
+                }
+                catch (Exception)
+                {
+                    DeleteFile(target, false);
+                }
+                if (top) Dispatcher.BeginInvoke(Println, console, "已删除病毒文件：" + target.FullName);
+            });
+            TASKS.Add(task);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -52,71 +81,21 @@ namespace FoshanVirusKiller
 
         private void KillTask()
         {
+            // 初始化启动
+            TASKS.Clear();
             Dispatcher.BeginInvoke(EnableButton, killer, false);
             Dispatcher.BeginInvoke(ClearText, console);
-            HashSet<string> files = new HashSet<string>();
 
+            // 检查系统进程
             Dispatcher.BeginInvoke(Println, console, "正在检查系统进程...");
             KillProcess();
-            
-            // C:\Users\Himmelt\AppData\Roaming\Microsoft\Office
-            //foreach (var user in Directory.GetDirectories("C:\\Users"))
-            //{
-            //    files.Add(user + "\\AppData\\Roaming\\Microsoft\\Office\\rundll32.exe");
-            //}
-            // 获取当前目录
-            string currentPath = Directory.GetCurrentDirectory();
-            // 获取驱动器
-            // 遍历文件夹 和 文件
-            if (new DriveInfo(currentPath).DriveType == DriveType.Removable)
-            {
-                Dispatcher.BeginInvoke(Println, console, "当前运行路径是U盘，将尝试恢复所有隐藏文件...");
-                foreach (var dir in Directory.GetDirectories(currentPath))
-                {
-                    try
-                    {
-                        Dispatcher.BeginInvoke(Println, console, "正在恢复文件夹：" + dir);
-                        new DirectoryInfo(dir).Attributes = FileAttributes.Normal;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-                foreach (var file in Directory.GetFiles(currentPath))
-                {
-                    try
-                    {
-                        File.SetAttributes(file, FileAttributes.Normal);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-            }
-            files.UnionWith(Directory.GetFiles(currentPath));
-            foreach (var path in files)
-            {
-                try
-                {
-                    FileStream file = File.OpenRead(path);
-                    string hash = BitConverter.ToString(SHA1.ComputeHash(file));
-                    file.Close();
-                    Dispatcher.BeginInvoke(Println, console, "正在检查：" + path);
-                    if (VHASH.Contains(hash))
-                    {
-                        Dispatcher.BeginInvoke(Println, console, "发现病毒：" + path);
-                        File.SetAttributes(path, FileAttributes.Normal);
-                        File.Delete(path);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    //Dispatcher.BeginInvoke(Println, console, "无法访问目标文件：" + e.Message);
-                }
-            }
+
+            // 检查可移动磁盘
+            KillUSBDisk();
+
+            Task.WaitAll(TASKS.ToArray());
+
+            // 查杀完毕
             Dispatcher.BeginInvoke(Println, console, "查杀完毕！");
             Dispatcher.BeginInvoke(EnableButton, killer, true);
         }
@@ -129,28 +108,85 @@ namespace FoshanVirusKiller
                 try
                 {
                     FileInfo info = new FileInfo(process.MainModule.FileName);
+                    //Dispatcher.BeginInvoke(Println, console, "正在检查进程：" + info.Name);
                     if (info.Exists && SIZES.Contains(info.Length))
                     {
-                        //Dispatcher.BeginInvoke(Println, console, "进程：" + path);
                         FileStream file = info.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
                         string hash = BitConverter.ToString(SHA1.ComputeHash(file));
                         file.Close();
                         if (VHASH.Contains(hash))
                         {
-                            Dispatcher.BeginInvoke(Println, console, "发现病毒进程：" + info.Name);
-                            process.Kill();
-                            info.Attributes = FileAttributes.Normal;
-                            info.Delete();
-                            Dispatcher.BeginInvoke(Println, console, "已删除病毒文件：" + info.FullName);
-                            //File.SetAttributes(path, FileAttributes.Normal);
-                            //File.Delete(path);
+                            try
+                            {
+                                process.Kill();
+                                Dispatcher.BeginInvoke(Println, console, "结束病毒进程：" + info.Name);
+                                DeleteFile(info, true);
+                            }
+                            catch (Exception e)
+                            {
+                                Dispatcher.BeginInvoke(Println, console, "病毒进程处理失败：" + info.FullName);
+                                Dispatcher.BeginInvoke(Println, console, e.Message);
+                            }
                         }
                     }
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    Console.WriteLine(e.Message);
+                    //Console.WriteLine(e.Message);
+                    //Dispatcher.BeginInvoke(Println, console, e.Message);
                 }
+            }
+        }
+
+        private void KillUSBDisk()
+        {
+            foreach (DriveInfo info in DriveInfo.GetDrives())
+            {
+                if (info.DriveType == DriveType.Removable)
+                {
+                    KillDirectory(info.RootDirectory);
+                }
+            }
+        }
+
+        private void KillDirectory(DirectoryInfo directory)
+        {
+            foreach (var dir in directory.GetDirectories())
+            {
+                try
+                {
+                    dir.Attributes = FileAttributes.Normal;
+                    KillDirectory(dir);
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine(e.Message);
+                    //Dispatcher.BeginInvoke(Println, console, e.Message);
+                }
+            }
+            foreach (var file in directory.GetFiles())
+            {
+                try
+                {
+                    file.Attributes = FileAttributes.Normal;
+                    TryKillVirus(file);
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine(e.Message);
+                    //Dispatcher.BeginInvoke(Println, console, e.Message);
+                }
+            }
+        }
+
+        private void TryKillVirus(FileInfo info)
+        {
+            if (info.Exists && SIZES.Contains(info.Length))
+            {
+                FileStream file = info.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                string hash = BitConverter.ToString(SHA1.ComputeHash(file));
+                file.Close();
+                if (VHASH.Contains(hash)) DeleteFile(info, true);
             }
         }
 
